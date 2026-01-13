12e20fe52871b52700fa9362cb3eb9b4171f49d4
GitHub Action <action@github.com>
2026-01-13 15:17:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/seansekora/terminalsmartpaste/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/seansekora/terminalsmartpaste/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.seansekora.terminalsmartpaste.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
